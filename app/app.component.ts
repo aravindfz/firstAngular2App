0b870629e6791f9e0d288e924d780db832568b3d
aravindfz <aravind.fz@gmail.com>
2017-01-08 21:45:09 +0530

    first Commit
    Made sure all the files are up and running

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'demo-app',
+    template: `
+        <h1>Angular2: First App </h1>
+    `
+})
+export class AppComponent { }
